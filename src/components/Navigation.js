eb28de2d5592c9086746f3e761f973156059a582
f-shakeri <56876476+f-shakeri@users.noreply.github.com>
2020-02-03 00:06:40 +0330

    increase app.js file size

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -9,6 +9,11 @@ import "./SkillCards.css";
 
 
 class Navigation extends Component {
+
+  constructor(props) {
+    super(props);
+  }
+
   render() {
     return (
       <div className="navigation">
@@ -18,12 +23,34 @@ class Navigation extends Component {
             <Nav.Link href="#skill"><h5>Skills</h5></Nav.Link>
           </Nav>
           <img
-          onClick={this.props.changeColor}
+          onClick={this.changeColor}
           className="paint" src={data.icons.paint} />
         </Navbar>
       </div>
     );
   }
+
+  changeColor = () => {
+    let myColors = [
+      "green", "blue", "yellow", "pink", "gray", "aqua", "magenta", "purple", "lightblue", "red",
+    ];
+    let  c1 = "", c2 = "", c3 = "";
+
+    c1 = myColors[Math.floor(Math.random() * myColors.length)];
+    c2 = myColors[Math.floor(Math.random() * myColors.length)];
+    c3 = myColors[Math.floor(Math.random() * myColors.length)];
+
+    while (c2 == c1)
+      c2 = myColors[Math.floor(Math.random() * myColors.length)];
+    while (c3 == c1 || c3 == c2)
+      c3 = myColors[Math.floor(Math.random() * myColors.length)];
+
+    this.props.parent.setState({
+      titleColor: c1,
+      contentColor: c2,
+      bcColor: c3
+    });
+  };
 }
 
 export default Navigation;
