1075f83a301ffd6421e6e3dca9d6e0b142efdb56
f-shakeri <56876476+f-shakeri@users.noreply.github.com>
2020-02-03 00:05:27 +0330

    increase app.js file size

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ import SnowStorm from "react-snowstorm";
 import "./App.css";
 
 class App extends Component {
-
-  myColors = [
-    "green", "blue", "yellow", "pink", "gray", "aqua", "magenta", "purple", "lightblue", "red",
-  ]
-
   constructor(props) {
     super(props);
     this.state = {
@@ -19,29 +14,15 @@ class App extends Component {
       contentColor: "aqua",
       bcColor: "pink"
     };
-
-    this.changeColor = this.changeColor.bind(this);
   }
-
-  changeColor = () => {
-    let c1 = this.myColors[Math.floor(Math.random() * this.myColors.length)];
-    let c2 = this.myColors[Math.floor(Math.random() * this.myColors.length)];
-    let c3 = this.myColors[Math.floor(Math.random() * this.myColors.length)];
-
-    this.setState({
-      titleColor: c1,
-      contentColor: c2,
-      bcColor: c3
-    });
-  };
 
   render() {
     return (
       <div className="App">
         <SnowStorm />
-        <Navigation changeColor = {this.changeColor} />
-        <TitleAndIcons colors = {this.state}/>
-        <AboutSection colors = {this.state}/>
+        <Navigation parent = {this} />
+        <TitleAndIcons colors = {this.state} />
+        <AboutSection colors = {this.state} />
         <SkillSection colors = {this.state} />
       </div>
     );
